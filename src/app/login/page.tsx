5024cb710bc86d42f4b71c8248ea63a162a0ca18
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-11-20 18:48:51 -0300

    feat: atualizando conteúdo do modal de informações

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,23 +8,15 @@ export default function Login() {
   return (
     <AuthPageTemplate title="Login">
       <ApiWarmupModal />
-      <CardSurface className="text-left max-w-[325px]" p={8}>
-        <h1 className="text-lg text-foreground font-medium leading-relaxed my-2">
-          Criei este módulo de autenticação Full Stack com os seguintes
-          tratamentos e retornos:
+      <CardSurface
+        className="text-left max-w-[325px] max-h-[400px] overflow-y-auto scrollbar-none"
+        p={8}
+      >
+        <h1 className="text-lg text-foreground font-medium leading-relaxed my-2 pl-[-16px]">
+          Criei este módulo de autenticação Full Stack (React/Next e NestJS) com
+          os seguintes tratamentos e retornos:
         </h1>
         <ul className="text-left list-disc">
-          <li>
-            Tratamento e retorno para credenciais inválidas{" "}
-            <span className="text-yellow-600 text-[2.2vh]">
-              (Experimente utilizar email e senha aleatórios)
-            </span>
-          </li>
-          <li>
-            Verificação de formato de e-mail válido (tanto no front-end, quanto
-            detalhadamente no back-end (API), mais detalhadamente com
-            bibliotecas/decorators utilizados no NestJS)
-          </li>
           <li>
             <span className="text-green-700  font-extrabold">
               Para um Login Válido:
@@ -34,9 +26,26 @@ export default function Login() {
             <br />
             <strong>senha:</strong> admin123
           </li>
+          <li>
+            Tratamento e retorno para credenciais inválidas{" "}
+            <span className="text-yellow-700 text-[2.2vh]">
+              (Experimente utilizar email e senha aleatórios)
+            </span>
+          </li>
+          <li>
+            Verificação de formato de e-mail válido (tanto no front-end, quanto
+            detalhadamente no back-end (API), mais detalhadamente com
+            bibliotecas/decorators utilizados no NestJS){" "}
+            <span className="text-yellow-700 text-[2.2vh]">
+              (Inserir um e-mail com formato inválido será reconhecido ou pelo
+              componente de form front-end ou pelo back-end)
+            </span>
+          </li>
         </ul>
       </CardSurface>
-      <LoginCard className="h-full w-full max-w-[500px]" />
+      <LoginCard className="h-full w-full max-w-[500px] max-h-[400px]" />
+      <br />
+      <br />
     </AuthPageTemplate>
   );
 }
